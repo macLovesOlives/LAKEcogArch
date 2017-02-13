61c1978da88fc2485756bd686243972c68a0b246
mackenzieostler <mackenzie.ostler@gmail.com>
2017-02-13 11:33:08 -0500

    Updated Owl

--- drawtheowl.py
+++ drawtheowl.py
@@ -7,35 +7,43 @@ import lake
 # if a circle exists
 #   draw a second circle
 # if two circles exist
-#   draw the fucking owl
+#   get sh*t done
+# if you got sh*t done AND you drew two circles
+#   draw the f*cking owl
 
 draw_head = Production(
     Precondition(
-        set(),
         {
             Node('Blank Slate'),
             Arrow(
                 (
-                Node('Is'),
-                Node('Paper'),
-                Node('Blank')
+                    Node('Is'),
+                    Node('Paper'),
+                    Node('Blank')
                 )
-                )
+            )
         }
     ),
-    Action({Node('Drawing circle')})
+    Action({Node('Drawing circle')},
+           {Node('Blank Slate'),
+            Arrow((Node('Is'), Node('Paper'), Node('Blank'))),
+            Node('Blank Slate'),
+            Arrow((Node("Is"),
+                   Node("Paper"),
+                   Node("Blank"))
+                  )
+            })
 )
 
 draw_body = Production(
     Precondition(
-        set(),
         {Node('Drawing circle')}),
     Action({Node('Drawing second circle')}))
 
 owl = Production(
     Precondition(
         {Node('Drawing circle'), Node('Drawing second circle'),
-         Node('Getting sh*t done.')}),
+         Node('Getting SH*T done.')}),
     Action(
         {Node('Draw the f*cking owl')}))
 
@@ -46,26 +54,33 @@ done = Production(
         {Node('Getting sh*t done.')}))
 
 
-productions = {draw_body, draw_head, owl}
+more_done = Production(
+    Precondition(
+        {Node('Drawing circle'), Node('Drawing second circle')}),
+    Action(
+        {Node('Getting SH*T done.')}))
+
+productions = {draw_body, draw_head, owl, done, more_done}
 knowledge = Knowledge({Node("No initial system knowledge")})
 
 engine = lake.Mind("test_mind")
 
-pools =              {'Visuo-Spatial': 3, 'Motor/Planning': 3}
-capacities =         {'Visuo-Spatial': 5, 'Motor/Planning': 5}
-rates =              {'Visuo-Spatial': 2, 'Motor/Planning': 2}
-costs = {draw_body:  {'Visuo-Spatial': 1},
-         draw_head:  {'Visuo-Spatial': 1},
-         owl:        {'Visuo-Spatial': 1, 'Motor/Planning': 1},
-         done:       {'Visuo-Spatial': 1, 'Motor/Planning': 3}}
+pools = {'Visuo-Spatial': 3, 'Motor/Planning': 3}
+capacities = {'Visuo-Spatial': 5, 'Motor/Planning': 5}
+rates = {'Visuo-Spatial': 2, 'Motor/Planning': 2}
+costs = {draw_body: {'Visuo-Spatial': 1},
+         draw_head: {'Visuo-Spatial': 1},
+         owl: {'Visuo-Spatial': 1, 'Motor/Planning': 1},
+         done: {'Visuo-Spatial': 1, 'Motor/Planning': 3},
+         more_done: {'Visuo-Spatial': 3, 'Motor/Planning': 4}}
 priorities = {draw_body: 1,
               draw_head: 1,
               owl: 1,
-              done: 1}
+              done: 1,
+              more_done: 3}
 
 parameters = (pools, capacities, rates, costs, priorities)
 system = ProductionSystem(knowledge, productions, engine.bind, parameters)
-
 
 system.step()
 print "State of the Mentons in step 1: "
@@ -75,10 +90,10 @@ system.knowledge.add({Node('Blank Slate'),
                       Arrow((Node("Is"),
                              Node("Paper"),
                              Node("Blank")))})
-for i in range(3):
+for i in range(5):
     system.step()
-    print "State of the Mentons in step " + str(i+2) + ": "
+    print "State of the Mentons in step " + str(i + 2) + ": "
     for i in range(len(engine.mentonPools)):
         print(str(engine.mentonPools[i].name) + ": " + str(engine.mentonPools[i].mentons))
 
-print '\n'+system.log
+print '\n' + system.log
