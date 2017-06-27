fceef5d6c8e56d7a4e90735e402cf1e396d627d6
Mackenzie Ostler <mackenzie.ostler@shopify.com>
2017-06-27 08:25:41 -0400

    Final Honours Draft Update: Parser

--- parser.py
+++ parser.py
@@ -1,93 +1,139 @@
+# Written by Mackenzie Ostler (mackenzie.ostler@gmail.com) 2017
+# Does NOT currently work.  If needed contact me for more info.
+
 import json
 from ProductionSystem import *
 import lake
+import sys
 
-"""
-intended to parse a json to easily add and create models in lake...
-Not updated... nor accurate anymore.
-Keeping in case it is needed in future (quickly modifiable)
-"""
 
 class Parser:
     def __init__(self):
-        mind = None
-        pool = None
-        mentonCapacity  = None
-        replenishRate   = None
+        self.mind = "test_mind"
+        self.pools = []
+        self.gactions = []
+        self.gaction_list = []
+        self.knowledge = Knowledge({Node("No initial system knowledge")})
+        self.mentonCapacity = None
+        self.replenishRate = None
+        self.steps = 0
+        self.parameters = {}
 
-    def getFile(self, jsonFile):
-        with open(jsonFile) as data_file:
+    def get_file(self, json_file):
+        with open(json_file) as data_file:
             data = json.load(data_file)
         return data
 
-    def extractValues(self, data_file, gaction):
-        self.mind = data_file["mind"]["name"]
-        self.pool = data_file["mind"]["pool"]["pool_name"]
-        self.mentonCapacity = data_file["mind"]["pool"]["mentonCapacity"]
-        self.replenishRate = data_file["mind"]["pool"]["replenishRate"]
-        # gactions
-        counter = 0
-        while counter < len(data_file["mind"]["gaction"]):
-            goal = data_file["mind"]["gaction"][counter]["goal"]["name"]
-            goal_importance = data_file["mind"]["gaction"][counter]["goal"]["importance"]
+    def extract_values(self, data_file):
+        mind = data_file["mind"]
+        gactions = mind["gactions"]
+        pools = mind["pools"]
+        self.mind = mind["name"]
+        self.steps = mind["number of steps"]
 
-            action = data_file["mind"]["gaction"][counter]["action"]["name"]
-            action_cost = data_file["mind"]["gaction"][counter]["action"]["cost"]
-            action_importance = data_file["mind"]["gaction"][counter]["action"]["importance"]
+        for i in pools:
+            self.pools.append(i)
 
-            relation = data_file["mind"]["gaction"][counter]["relationship"]
-            diction = {"goal": goal, "goal_importance": goal_importance, "action": action, "action_cost": action_cost,
-                       "action_importance": action_importance, "relation": relation}
-            gaction.append(diction)
+        for i in gactions:
+            self.gactions.append(i)
 
-            counter += 1
+            name = i["name"]
+            precondition = []
+            for node in i["precondition"]:
+                print(node)
+                if precondition != "":
+                    con = str(node["node"])
+                    precondition.append(con)
+                else:
+                    con = str(node["node"])
+                    precondition.append(con)
+            start = []
+            for node in i["action"][0]["start"]:
+                if start != "":
+                    con = str(node["node"])
+                    start.append(con)
+                else:
+                    con = str(node["node"])
+                    start.append(con)
+            stop = []
+            if len(i["action"][1]["stop"]) == 0:
+                # print(Node(x) for x in precondition)
+                print('%s = Production(Precondition({Node("%s")}),Action({Node("%s")}))' %(name, precondition[0], start[0]))
+                exec('%s = Production(Precondition({Node("%s")}),Action({Node("%s")}))' %(name, precondition[0], start[0]))
+                self.gaction_list.append(Production(Precondition({Node(precondition[0])}), Action({Node(start[0])})))
+            else:
+                for node in i["action"][1]["stop"]:
+                    if stop != "":
+                        con = str(node["node"])
+                        stop.append(con)
+                    else:
+                        con = str(node["node"])
+                        stop.append(con)
+                print("%s = Production(Precondition({Node('%s')}),Action({Node('%s')},{Node('%s')}))" %(name, precondition[0], stop[0], start[0]))
+                exec("%s = Production(Precondition({Node('%s')}),Action({Node('%s')},{Node('%s')}))" %(name, precondition[0], stop[0], start[0]))
+                self.gaction_list.append(Production(Precondition({Node(precondition[0])}), Action({Node(stop[0])}, {Node(start[0])})))
 
-    def creatGactions(self, gactionlist):
-        lake = lake
+    def create_mind(self):
+        lake.Mind(self.mind)
 
-        # create mind
-        mind = lake.Mind(self.mind)
-        pool = lake.createMentonPool(self.pool, mind, self.mentonCapacity, self.replenishRate)
-        chunkList = []
-        for gaction in range(len(gactionlist)):
-            # create goal
-            lake_goal = lake.createGoal(mind, gactionlist[gaction]['goal'], gactionlist[gaction]['goal_importance'])
-            # create action
-            lake_action = lake.createAction(mind, gactionlist[gaction]['action'], gactionlist[gaction]['goal'],
-                                            gactionlist[gaction]['action_cost'],
-                                            gactionlist[gaction]['action_importance'])
-            # create chunk
-            lake_chunk = lake.createChunk(mind, lake_action, gactionlist[gaction]['relation'], lake_goal)
-            chunkList.append(lake_chunk)
+    def create_pools(self):
+        self.parameters["pools"] = {}
+        self.parameters["capacity"] = {}
+        self.parameters["rates"] = {}
 
-            return chunkList
+        for i in self.pools:
+            name = str(i["name"])
+            cap = i["capacity"]
+            rate = i["rates"]
+            initial = i["initial"]
 
-    def useProductionSystem(self, chunkList):
-        productions = {}
-        for i in range(len(chunkList)):
-            prod = Production(Precondition(set(),
-                                           Arrow((
-                                               Node(chunkList[i].relation), Node(chunkList[i].thingx.name),
-                                               Node(chunkList[i].thingy.name)
-                                           )),
-                                           Action(({Node(chunkList[i].thingx.name)}))))
+            self.parameters["pools"][name] = initial
+            self.parameters["capacity"][name] = cap
+            self.parameters["rates"][name] = rate
 
-            print("dictionary index")
-            print(chunkList[i].thingy.name)
-            productions[chunkList[i].thingy.name] = prod
+    def create_params(self):
+        self.parameters["costs"] = {}
+        self.parameters["priorities"] = {}
 
+        for i in self.gactions:
+            name = str(i["name"])
+            priorities = i["priority"]
+            costs = {}
+            for j in i["costs"]:
+                k = (str(j.keys()[0]))
+                v = j[k]
+                costs.update({k: v})
+
+            self.parameters["costs"][name] = costs
+            self.parameters["priorities"][name] = priorities
+
+
+def main():
+    p = Parser()
+    file = str(sys.argv[1])
+    f = p.get_file(file)
+    p.extract_values(f)
+    p.create_mind()
+    p.create_pools()
+    p.create_params()
+
+    # for key in p.parameters:
+    #     print(str(key) + ": " + str(p.parameters[key]))
+
+    engine = lake.Mind(str(p.mind))
+    par = p.parameters
+    system = ProductionSystem(p.knowledge, p.gaction_list, engine.bind, (par["pools"], par["capacity"], par["rates"], par["costs"], par["priorities"]))
+    # for i in range(len(engine.mentonPools)):
+    #     print(str(engine.mentonPools[i].name) + ": " + str(engine.mentonPools[i].mentons))
+
+    system.knowledge.add({Arrow((Node("Is"), Node("Paper"), Node("Blank")))})
+    for i in range(5):
+        system.step()
+        print "\nState of the Mentons in step " + str(i + 2) + ": "
+        for j in range(len(engine.mentonPools)):
+            print(str(engine.mentonPools[j].name) + ": " + str(engine.mentonPools[j].mentons))
+
+    print '\n' + system.log
 
 if __name__ == "__main__":
-    parser = Parser()
-    f = parser.getFile("testFile.json")  # this name should be passed in from cmd
-
-    gaction = []
-
-    parser.extractValues(f, gaction)
-    chunkList = parser.creatGactions(gaction)
-    productions = parser.useProductionSystem(chunkList)
-    engine = lake.Mind()
-
-    priorities = {}
-
-    system = ProductionSystem(knowledge, productions, engine.bind, )
+    main()
